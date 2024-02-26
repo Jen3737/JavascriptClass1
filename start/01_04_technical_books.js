16346661e496db6bbb4c75c603c777fe3f2b0ef8
Jen3737 <160965254+Jen3737@users.noreply.github.com>
2024-02-26 17:08:23 +0000

    finished exercise

--- start/01_04_technical_books.js
+++ start/01_04_technical_books.js
@@ -1,33 +1,83 @@
 class Book {
-  constructor(title, author, ISBN, numCopies) {
-    this.title = title;
-    this.author = author;
-    this.ISBN = ISBN;
-    this.numCopies = numCopies;
+  constructor(title, author, ISBN, numCopies)
+  {
+    this.title = title,
+    this.author = author,
+    this.ISBN = ISBN,
+    this.numCopies = numCopies
   }
 
-  // Getter
   get availability() {
     return this.getAvailability();
   }
-
-  // Method
   getAvailability() {
-    if (this.numCopies === 0) {
-      return "Out of stock";
-    } else if (this.numCopies < 10) {
-      return "Low stock";
+    let num = this.numCopies
+    let status;
+    
+    switch (true) {
+      case num == 0:
+        status = "Availability: Out of stock"
+        break;
+      case num < 10:
+        status = "Availability: Stock is low"
+        break;
+      default:
+        status = "Availability: In stock"
     }
-    return "In stock";
+    return status;
+  }
+  sell(sellResult, numSold = 1) {
+    let num = this.numCopies;
+    
+    if (num >= numSold) { 
+      sellResult.newNum = (num - numSold)
+      sellResult.message = "Sold!  There are " + sellResult.newNum + " copies left."
+    } else {
+      sellResult.newNum = num
+      sellResult.message = "Not enough books in stock!  There are only " + sellResult.newNum + " copies left."
+    }
+    console.log(sellResult.message)
+    this.numCopies = sellResult.newNum;
+    return sellResult
   }
 
-  sell(numCopiesSold = 1) {
-    this.numCopies -= numCopiesSold;
-  }
-  
-  restock(numCopiesStocked = 5) {
+ restock(numCopiesStocked = 5) {
     this.numCopies += numCopiesStocked;
+    console.log("Restocked!  There are now " + this.numCopies + " copies in stock.")
   }
 }
 
+  class sellResult  {
+    constructor (newNum, sellMsg)
+    {
+      this.newNum = newNum
+      this.sellMsg = sellMsg
+    }
+  }
+class TechnicalBook extends Book {
+  constructor(title, author, ISBN, numCopies, edition) {
+    super(title, author, ISBN, numCopies);
+    this.edition = edition;
+  }
+
+  getEdition() {
+    return `The current version of this book is ${this.edition}`;
+  }
+}  
+const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+console.log(HungerGames.availability);
+
+HungerGames.sell(sellResult, 2);
+console.log(HungerGames.availability);
+HungerGames.restock(4);
+console.log(HungerGames.availability);
+HungerGames.sell(sellResult, 7);
+console.log(HungerGames.availability);
+HungerGames.restock(15);
+console.log(HungerGames.availability);
+
+const CrackingTheCodingInterview = new TechnicalBook("Cracking The Coding Interview", "Gayle Laackmann McDowell", 1209123, 7, "2.3");
+
+console.log(CrackingTheCodingInterview.availability);
+console.log(CrackingTheCodingInterview.getEdition());
 // Write your code here
